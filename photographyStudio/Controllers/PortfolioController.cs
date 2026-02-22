ea73dc35e29be0fa0cb3686c9df8aa9e08492109
Kex6awn <fferdinard1@gmail.com>
2026-02-22 15:50:28 -0600

    uploadmodel and upload view files

--- photographyStudio/Controllers/PortfolioController.cs
+++ photographyStudio/Controllers/PortfolioController.cs
@@ -1,4 +1,7 @@
 ﻿using KxnPhotoStudio.Data;
+using KxnPhotoStudio.Models;
+using KxnPhotoStudio.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +22,51 @@ namespace KxnPhotoStudio.Controllers
             var photos = await _context.Photos.Include(p => p.Category).OrderByDescending(p => p.CreatedDate).ToListAsync();
 
             return View(photos);
+        }
+
+        // GET: Upload
+        public async Task<IActionResult> Upload()
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: Upload
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(PhotoUploadViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = new SelectList(_context.Categories, "CategoryId", "Name");
+                return View(model);
+            }
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ImageFile.FileName;
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await model.ImageFile.CopyToAsync(fileStream);
+            }
+
+            var photo = new Photo
+            {
+                Title = model.Title,
+                Description = model.Description,
+                CategoryId = model.CategoryId,
+                ImagePath = "/uploads" + uniqueFileName
+            };
+
+            _context.Photos.Add(photo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ photographyStudio/Models/ViewModels/PhotoUploadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KxnPhotoStudio.Models.ViewModels
+{
+    public class PhotoUploadViewModel
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description {  get; set; } = string.Empty;
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public IFormFile ImageFile { get; set; } = null!;
+    }
+}
