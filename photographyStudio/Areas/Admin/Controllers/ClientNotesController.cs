af5c15b008055e64ccfad8a8ae3e53963da8c036
Kex6awn <fferdinard1@gmail.com>
2026-08-04 05:28:43 -0500

    Feat: Added Client notes management controller

---
+++ photographyStudio/Areas/Admin/Controllers/ClientNotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KxnPhotoStudio.Areas.Admin.Controllers
+{
+    public class ClientNotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
